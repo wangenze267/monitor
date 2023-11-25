5b86da0abadca8ecdc6115dcc6b920a95255b223
wangenze <15041567431@126.com>
2023-11-25 17:12:01 +0800

    :sparkles: 增加计算页面性能的工具函数

---
+++ funcs/performance.js
@@ -0,0 +1,47 @@
+// 处理计算页面性能的函数
+
+
+const processData = (p) => {
+  const data = {
+    prevPage: p.fetchStart - p.navigationStart, // 上个页面到这个页面的时长
+    redirect: p.redirectEnd - p.redirectStart, // 重定向时长
+    dns: p.domainLookupEnd - p.domainLookupStart, // dns 解析时长
+    connect: p.connectEnd - p.connectStart, // tcp 连接时长
+    // 从请求到响应的时长
+    send: p.responseEnd - p.requestStart, // 响应结束到请求结束
+    ttfb: p.responseStart - p.navigationStart, // 首字节接收到的时长
+    domReady: p.domInteractive - p.domLoading, // dom 准备时长
+    // 白屏时间
+    whiteScreen: p.domLoading - p.navigationStart,
+    // dom 解析时间
+    dom: p.domComplete - p.domLoading,
+    // onload 执行时间
+    load: p.loadEventEnd - loadEventStart,
+    // 总计
+    total: p.loadEventEnd - p.navigationStart,
+  }
+  return data
+}
+
+const load = (cb) => {
+  let timer
+  const check = () => {
+    if(performance.timing.loadEventEnd) {
+      clearTimeout(timer)
+      cb()
+    }else {
+      timer = setTimeout(check, 100)
+    }
+  }
+  window.addEventListener('load', check, false)
+}
+
+export default {
+  init(cb) {
+    load(() => {
+      const perfData = performance.timing
+      const data = processData(perfData)
+      cb(data)
+    })
+  }
+}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// 监控页面性能
-
+import Perf from './funcs/performance.js'
+// 监控页面性能 
+// 即 算时间差 （各种）
+// Performance Api
+Perf.init((data) => {
+  console.log(data)
+})
 // 监控页面静态资源加载情况
 
 // 监控请求情况（Ajax发送情况）

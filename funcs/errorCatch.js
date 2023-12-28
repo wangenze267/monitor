824e52f48eba95533526ad7671d1ae95332faffb
wangenze <15041567431@126.com>
2023-12-28 10:37:47 +0800

    :sparkles: 增加错误捕获

---
+++ funcs/errorCatch.js
@@ -0,0 +1,23 @@
+export default {
+  init(cb) {
+    // 监听不到404
+    // 404 需要去监听 window.addEventListener('error')
+    // promise 失败了也不能通过 onerror 捕获
+    window.onerror = function (message, source, lineno, colno, error) {
+      console.dir(error)
+      const info = {
+        message: error.message,
+        name: error.name,
+      }
+      const stack = error.stack
+      const matchUrl = stack.match(/http:\/\/[^\n]/)[0]
+      info.fileName = matchUrl.match(/http:\/\/(?:\S*)\.js/)[0]
+      const [, row, column] = matchUrl.match(/:(\d+):(\d+)/)
+      info.row = row
+      info.column = column
+      // 上线的时候代码会压缩 source-map
+      // 需要通过映射代码反解，找到真实报错
+      cb(info)
+    }
+  }
+}
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import perf from './funcs/performance.js'
 import resource from './funcs/resource.js'
 import tool from './utils/index.js'
 import xhr from './funcs/xhr.js'
+import errorCatch from './funcs/errorCatch.js'
 
 // 导出
 export default {
   perf,
   resource,
   tool,
-  xhr
+  xhr,
+  errorCatch
 }
